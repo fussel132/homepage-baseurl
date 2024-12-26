df672a89ac9e777d6afc776061c5c380df56b361
Fussel132 <lukaskrome@gmail.com>
2024-12-26 04:54:34 +0100

    add base url

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,9 +9,9 @@ export default function Document() {
           content="A highly customizable homepage (or startpage / application dashboard) with Docker and service API integrations."
         />
         <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="manifest" href="/site.webmanifest?v=4" crossOrigin="use-credentials" />
-        <link rel="preload" href="/api/config/custom.css" as="style" />
-        <link rel="stylesheet" href="/api/config/custom.css" /> {/* eslint-disable-line @next/next/no-css-tags */}
+        <link rel="manifest" href="/admin/site.webmanifest?v=4" crossOrigin="use-credentials" />
+        <link rel="preload" href="/admin/api/config/custom.css" as="style" />
+        <link rel="stylesheet" href="/admin/api/config/custom.css" /> {/* eslint-disable-line @next/next/no-css-tags */}
       </Head>
       <body>
         <Main />
